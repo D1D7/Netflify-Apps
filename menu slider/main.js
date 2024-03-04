5cff74cf424352a5b612edcc75d61677f24aed06
D1D7 <152475650+D1D7@users.noreply.github.com>
2024-03-04 23:14:16 +0400

    Add files via upload

---
+++ menu slider/main.js
@@ -0,0 +1,7 @@
+function toggleNav() {
+    var updateElement = document.getElementById("menu-icon");  
+updateElement.classList.toggle("open"); 
+
+var updateElement = document.getElementById("slider");
+slider.classList.toggle("open");
+} 
